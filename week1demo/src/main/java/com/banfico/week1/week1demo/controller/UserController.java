02e8f498bef66eb09f205cfa56cafd143899b82e
Tilak-Codex <tilak25rj@gmail.com>
2026-07-21 18:18:01 +0530

    Adding List of users using POST and getting response

--- week1demo/src/main/java/com/banfico/week1/week1demo/controller/UserController.java
+++ week1demo/src/main/java/com/banfico/week1/week1demo/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.banfico.week1.week1demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +23,21 @@ public class UserController {
             
         userRepo.save(user);
 
+
+
+        // To get the user inserted as response we can use the below code instead of void
+        /*  public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userRepo.save(user);
+            return ResponseEntity.ok(savedUser);
+        }
+            */
 }
+
+    @PostMapping("/createAll")
+    public ResponseEntity<List<User>> createListOfUsers(@RequestBody List<User> users) {
+        List<User>savedUsers= userRepo.saveAll(users);
+        
+        return ResponseEntity.ok(savedUsers);
+    }
+
 }
